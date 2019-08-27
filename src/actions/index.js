77ae0682da418be1585ebdf24c00a4b3bdaecd34
wang790222 <wang790222@gmail.com>
2019-08-27 14:43:05 -0400

    Combine Two Actions to One

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,21 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // Original Version
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // Chain Version
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+}
+
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
